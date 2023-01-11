222bc672baa8e235a30043f71e441b895cac41b6
Hamed Jafari <hamedjix@gmail.com>
2023-01-10 23:06:06 -0500

    add workout model

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Workout = require("../models/workoutModel");
 
 //Router
 const router = express.Router();
@@ -13,8 +14,15 @@ router.get("/:id", (req, res) => {
   res.json({ message: "Get a single workout" });
 });
 // Post a new Workout
-router.post("/", (req, res) => {
-  res.json({ message: "Post a new workout" });
+router.post("/", async (req, res) => {
+  const { title, reps, load } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 // Delete a Workout
 router.delete("/:id", (req, res) => {
