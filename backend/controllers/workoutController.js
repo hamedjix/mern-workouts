50aaa2ea2fc6c111531f9d39113d788ed27ebd1e
Hamed Jafari <hamedjix@gmail.com>
2023-01-11 12:47:24 -0500

    completed workout controller

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -33,9 +33,29 @@ const createWorkout = async (req, res) => {
 };
 // delete a workout
 
-const deleteWorkout = async (req, res) => {};
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+  const workout = await Workout.findOneAndDelete({ _id: id });
+  if (!workout) {
+    return res.status(400).json({ error: "no such workout" });
+  }
+  res.status(200).json(workout);
+};
 // update a workout
-const updateWorkout = async (req, res) => {};
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+  const workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body });
+  if (!workout) {
+    return res.status(400).json({ error: "no such workout" });
+  }
+  res.status(200).json(workout);
+};
 
 module.exports = {
   getAllWorkouts,
